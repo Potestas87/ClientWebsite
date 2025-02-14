59e18aa1abce4659fa4c91e7298a04fdf513e8f2
Daynen Hinckley <daynenhinckley@Daynens-MacBook-Pro.local>
2025-02-14 03:30:17 -0500

    Added embedded email and phone calling links

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -2,9 +2,9 @@ function ContactMe() {
     return (
       <div className="background">
         <h2>Contact Me</h2>
-        <p>Phone Number: 208-270-9544</p>
-        <p>Email: Outoftimeproductions@gmail.com</p>
-        <a href="/Troys resume final.pdf" download="/Troys resume final.pdf">
+        <a href="tel:12082709544" style={{ fontSize: "18px", color: "blue", textDecoration: "underline" }}>Phone Number: 208-270-9544</a><br/ >
+        <a href="mailto:outoftimeproductions@gmail.com" style={{ fontSize: "18px", color: "blue", textDecoration: "underline" }}>Email: Outoftimeproductions@gmail.com</a><br/ >
+        <a href="/Troys resume final.pdf" download>
           <button>Download My Resume Here</button>
         </a>
       </div>
