1d33690d2b06f2038509f11f6cc9fd3fc380d5d6
Daynen Hinckley <daynenhinckley@Daynens-MacBook-Pro.local>
2025-02-14 03:17:15 -0500

    Added embedded google drive videos to the portfolio section for the client

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,7 +2,89 @@ function Portfolio() {
     return (
       <div className="background">
         <h2>Portfolio</h2>
-        <p>I have had the chance to work on many amazing projects in my career. Let me walk you throough some of them!</p>
+        <p>I have had the chance to work on many amazing projects in my career. Let me walk you through some of them!</p>
+
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+          <iframe
+               src="https://drive.google.com/file/d/17LRZVNvizxJ1roxDaMNQq08k6g01NLd4/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="Troy Demo Reel"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1klqaxjM3KsK2x_Fj76_jRCfYpNAMcoPd/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="2022 provex"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1h1J5_gNXVg1jgZYUobqq1dPubdKgEXlD/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="2022 Launch Video"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1I7E4YGvDHMLevX7phZyY28AAh65rXcy9/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="Active Grandma"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1QHfE7e7y2o9jxSOgGhhWg8TtcJ3BIv7E/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="2022 Presidents Club"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/12Y1ZUB8sesEzgHh6awmKAAIn8Q9g1HNi/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="McKay: what we have learned"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1oexTxuFGTJGQFxc7vJsFlBrOoejZVtjq/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="John Dufner 2019 Documonial"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1OpS4HyXFDWG--8OFOP7xTlH0i8IYy7sL/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="Mckay Short Segment"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/15M_hq6fOiD4WDvm10qi9DWlQdVobcOHh/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="Audrey Biggs"
+          ></iframe>
+
+          <iframe
+               src="https://drive.google.com/file/d/1BgBjmKLNibBBdApOXJLk8W3eHuTH9Zkc/preview"
+               width="640"
+               height="360"
+               allow="autoplay"
+               title="NIL promo"
+          ></iframe>
+        </div>
       </div>
     );
   }
